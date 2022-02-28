b613633e1c6d87f748a5613e33143fbcee09c704
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2022-02-28 12:41:51 -0300

    Add files via upload

---
+++ Seleção de Cores HSV.py
@@ -0,0 +1,14 @@
+from tkinter import*
+from tkinter.colorchooser import*
+import tkinter.messagebox
+window = Tk()
+window.title("Seleção de Cores")
+window.geometry("300x50")
+window.config(background='#c0c0c0')
+
+def escolha_cor():
+    color = askcolor()
+    print(color)
+
+Button(text='Selecione a cor.', command=escolha_cor).pack()
+mainloop()
